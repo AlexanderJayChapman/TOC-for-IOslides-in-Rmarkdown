f101c917b2e7fb43928d99304fb0267407c4d476
Sharon <s.klinkenberg@uva.nl>
2016-08-16 22:13:40 +0200

    Added ToC generator and CSS

---
+++ TOC_generator.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+  TableOfContents();
+}
+);                        
+
+function TableOfContents(container, output) {
+
+var output = output || '#toc';
+
+// Get all elements with class: section or subsection
+idfromclass = document.querySelectorAll('.section, .subsection');
+
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    // Iterate through all found elements
+    for(var i = 0; i < idfromclass.length; i++) {
+      
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        var id = idfromclass[i].id
+        
+        // Replace - in id with whitespace
+        var titleText = id.replace(/-/gi, " ");
+        
+        // Add text to list element
+        item.appendChild(document.createTextNode(titleText));
+        
+        // Add subsection classes
+        if(idfromclass[i].className == "section")    { item.className = "section";    } 
+        if(idfromclass[i].className == "subsection") { item.className = "subsection"; }
+        
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+// Return generated HTML to toc div in slide
+document.querySelector(output).innerHTML = list.innerHTML;
+
+// Generate instruction message if no classes are defined
+if (idfromclass.length == 0) { document.querySelector(output).innerHTML = "Add {.section} or {.subsection} to slide name to generate TOC"; }  
+
+};
