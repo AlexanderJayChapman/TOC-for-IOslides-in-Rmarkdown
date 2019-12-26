c822afe67447bdb903c3ccac72aaf018ade18cdd
ShKlinkenberg <s.klinkenberg@gmail.com>
2019-12-26 20:14:55 +0100

    its not working

--- TOC2.js
+++ TOC2.js
@@ -1,35 +1,34 @@
-window.onload = function () {
-    var toc = "";
-    var level = 0;
+var TOC = document.getElementById('toc'),
+headings = document.querySelectorAll('.section, .subsection'),
+parentLevel = 1,
+cursorNode = TOC;
 
-    document.getElementByClass("#section").innerHTML =
-        document.getElementByClass("#section").innerHTML.replace(
-            /<h([\d])>([^<]+)<\/h([\d])>/gi,
-            function (str, openLevel, titleText, closeLevel) {
-                if (openLevel != closeLevel) {
-                    return str;
-                }
+for (var i = 0, len = headings.length ; i < len ; ++i) {
+	var currentHeading = headings[i];
+	var newLevel = parseInt(currentHeading.tagName.substr(1,1));
+	var diff = newLevel - parentLevel;
+	if (diff > 0) {
+		var containerLiNode = cursorNode.lastChild;
+		var ulNode = document.createElement('UL')
+		containerLiNode.appendChild(ulNode);
+		cursorNode = ulNode;
+		parentLevel = newLevel;		
+	}
+	if (diff < 0) {
+		while (0 !== diff++) {
+			cursorNode = cursorNode.parentNode.parentNode;
+		}
+		parentLevel = newLevel;
+	}
 
-                if (openLevel > level) {
-                    toc += (new Array(openLevel - level + 1)).join("<ul>");
-                } else if (openLevel < level) {
-                    toc += (new Array(level - openLevel + 1)).join("</ul>");
-                }
-
-                level = parseInt(openLevel);
-
-                var anchor = titleText.replace(/ /g, "_");
-                toc += "<li><a href=\"#" + anchor + "\">" + titleText
-                    + "</a></li>";
-
-                return "<h" + openLevel + "><a name=\"" + anchor + "\">"
-                    + titleText + "</a></h" + closeLevel + ">";
-            }
-        );
-
-    if (level) {
-        toc += (new Array(level + 1)).join("</ul>");
-    }
-
-    document.getElementById("toc").innerHTML += toc;
-};
+	var liNode = document.createElement('LI');
+	if (currentHeading.hasAttribute('id')) {
+		var link = document.createElement('A');
+		link.setAttribute('href', '#' + currentHeading.getAttribute('id'));
+		link.appendChild(document.createTextNode(currentHeading.textContent))
+		liNode.appendChild(link);
+	} else {
+		liNode.appendChild(document.createTextNode(currentHeading.textContent));
+	}
+	cursorNode.appendChild(liNode);
+}
